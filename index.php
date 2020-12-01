e31a9d03d62d2f5a05e2a9be31c7079613b75b61
Deddy Romnan <romnandeddy@gmail.com>
2020-12-01 15:45:35 +0700

    Initial commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php 
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Digital Library</title>
+</head>
+<body>
+	<main>
+		<h1>Digital Library</h1>
+		<h2>Books list goes here</h2>
+		<a href="login.php">Login</a>
+	</main>
+</body>
+</html>
